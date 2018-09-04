35f12d225a6ae0fd31cc041e2af05edccf6b7c34
EVGENY <evro23x@mail.ru>
2018-09-04 08:56:04 +0300

    add calculateTheDistance

---
+++ src/Distance.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace evro23x;
+
+class Distance
+{
+    public function calculateTheDistance ($arg1, $arg2, $arg3, $arg4) {
+
+        // перевести координаты в радианы
+        $lat1 = $arg1 * M_PI / 180;
+        $lat2 = $arg3 * M_PI / 180;
+        $long1 = $arg2 * M_PI / 180;
+        $long2 = $arg4 * M_PI / 180;
+
+        // косинусы и синусы широт и разницы долгот
+        $cl1 = cos($lat1);
+        $cl2 = cos($lat2);
+        $sl1 = sin($lat1);
+        $sl2 = sin($lat2);
+        $delta = $long2 - $long1;
+        $cdelta = cos($delta);
+        $sdelta = sin($delta);
+
+        // вычисления длины большого круга
+        $y = sqrt(pow($cl2 * $sdelta, 2) + pow($cl1 * $sl2 - $sl1 * $cl2 * $cdelta, 2));
+        $x = $sl1 * $sl2 + $cl1 * $cl2 * $cdelta;
+
+        //
+        $ad = atan2($y, $x);
+        // Радиус земли = 6372795
+        $dist = $ad * 6372795;
+
+        return round($dist);
+    }
+}
+
+
+
+
+$lat1 = 55.7414955;
+$long1 = 37.6050417;
+$lat2 = 55.7387655;
+$long2 = 37.6121013;
+
+$result = calculateTheDistance($lat1, $long1, $lat2, $long2);
+
+echo "Расстояние между координатами $lat1,$long1 и $lat2,$long2 равно $result метра(ов)\n";
